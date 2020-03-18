56ed569e0adb424f6a74613ea3dda61ae29c8be9
Morjus <morjus@yandex.ru>
2020-03-18 23:10:47 +0300

    fixed code

--- test_main_page.py
+++ test_main_page.py
@@ -4,21 +4,23 @@ from .pages.basket_page import BasketPage
 import pytest
 import time
 
-@pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
 
-@pytest.mark.skip
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_can_go_to_login_page(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+
+    def test_guest_should_see_login_link(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 @pytest.mark.skip
 def test_guest_go_to_login_page_via_login_in_url(browser):
