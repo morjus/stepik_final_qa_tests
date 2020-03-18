4e8da9ac484862e70d6e7d1c4aa45e237e6b0e5a
Morjus <morjus@yandex.ru>
2020-03-18 19:14:15 +0300

    Added product page test

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,11 @@ class MainPageLocators():
 class LoginPageLocators():
 	LOGIN_FORM = (By.CSS_SELECTOR, 'button[value="Log In"]')
 	REGISTRATION_FORM = (By.CSS_SELECTOR, 'button[value="Register"]')
+
+class ProductPageLocators():
+	ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+	PRODUCT_COST = (By.CSS_SELECTOR, ".col-sm-6.product_main  p.price_color")
+	PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main :nth-child(1)")
+	PRODUCT_NAME_IN_MESSAGE = (By.CSS_SELECTOR, ".alertinner :first-child")
+	ADDED_TO_CART_MESSAGE = (By.CSS_SELECTOR, ".alertinner")
+	CART_COST = (By.CSS_SELECTOR, ".alert-info .alertinner :nth-last-child(1)")
---
+++ pages/product_page.py
@@ -0,0 +1,42 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+import math
+from selenium.common.exceptions import NoAlertPresentException
+
+
+class ProductPage(BasePage):
+    def add_to_cart(self):
+        add_to_cart_button = self.browser.find_element(
+            *ProductPageLocators.ADD_TO_CART_BUTTON)
+        add_to_cart_button.click()
+        self.solve_quiz_and_get_code()
+        self.should_see_success_message()
+        self.name_of_product_equal_to_name_in_success_message()
+        self.cost_of_product_equal_to_cart_cost()
+    
+    def should_see_success_message(self):
+        assert self.is_element_present(*ProductPageLocators.ADDED_TO_CART_MESSAGE), "No success message."
+
+    def name_of_product_equal_to_name_in_success_message(self):
+        name_of_product = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        name_in_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_MESSAGE).text
+        assert name_of_product == name_in_message, f'"{name_of_product}" not equal to "{name_in_message}" in success message.'
+
+    def cost_of_product_equal_to_cart_cost(self):
+        cost = self.browser.find_element(*ProductPageLocators.PRODUCT_COST).text
+        result_cost = self.browser.find_element(*ProductPageLocators.CART_COST).text
+        assert cost == result_cost, f'Result cost: "{result_cost}" in basket not equal to cost of product: "{cost}".'
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
