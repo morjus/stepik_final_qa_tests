ce81199e10f54c2990dfde871e68bbdd86b0d361
Morjus <morjus@yandex.ru>
2020-03-20 11:34:49 +0300

    hotfix

--- test_product_page.py
+++ test_product_page.py
@@ -45,7 +45,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
-@pytest.mark.xfail(reason="it's always fail")
+@pytest.mark.xfail(reason="Negative test-case. Actually should not disappear.")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -60,7 +60,7 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
-@pytest.mark.xfail(reason="it's always fail")
+@pytest.mark.xfail(reason="Negative test-case. Actually should not disappear.")
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
